87c36170730649d2cf4a24008854c6deb02653d9
subratb <subrat@live.co.uk>
2015-10-04 21:53:28 -0400

    Added Utility project to enable async stream reading based on SignalR implementation. Initial Check in. Does not build.

---
+++ Utility/IResponseExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public static class IResponseExtensions
+    {
+        public static Task<string> ReadAsString(this IResponse response, Func<ArraySegment<byte>, bool> onChunk)
+        {
+            if (response == null)
+            {
+                throw new ArgumentNullException("response");
+            }
+
+            var stream = response.GetStream();
+            var reader = new AsyncStreamReader(stream);
+            var result = new StringBuilder();
+            var resultTcs = new TaskCompletionSource<string>();
+
+            reader.Data = buffer =>
+            {
+                if (onChunk(buffer))
+                {
+                    result.Append(Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count));
+                }
+            };
+
+            reader.Closed = exception =>
+            {
+                response.Dispose();
+                resultTcs.SetResult(result.ToString());
+            };
+
+            reader.Start();
+
+            return resultTcs.Task;
+        }
+
+        public static Task<string> ReadAsString(this IResponse response)
+        {
+            // Read all chunks by default
+            return response.ReadAsString(chunk => true);
+        }
+    }
+
+}
