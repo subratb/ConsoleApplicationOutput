ee875a174876f851df161c809382c2543002b5c8
Subrat Bisoyi <subrat@live.co.uk>
2015-10-05 17:19:49 -0400

    Added other related artifacts to enable async stream capture. Refer to SignalR implementation.

---
+++ Utility/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.md in the project root for license information.
+using System;
+
+namespace Utility
+{
+    internal static class ExceptionsExtensions
+    {
+        internal static Exception Unwrap(this Exception ex)
+        {
+            if (ex == null)
+            {
+                return null;
+            }
+
+            var next = ex.GetBaseException();
+            while (next.InnerException != null)
+            {
+                // On mono GetBaseException() doesn't seem to do anything
+                // so just walk the inner exception chain.
+                next = next.InnerException;
+            }
+
+            return next;
+        }
+    }
+
+}
---
+++ Utility/StandardOutput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class StandardOutput:IResponse
+    {
+        StreamReader _standardOutput;
+        public StandardOutput(StreamReader standardOutput)
+        {
+            if (standardOutput == null)
+                throw new Exception("standardOutput cannot be null");
+            _standardOutput = standardOutput;
+        }
+
+        public void Dispose()
+        {
+            _standardOutput = null;
+        }
+
+        public Stream GetStream()
+        {
+            return this._standardOutput.BaseStream;
+        }
+    }
+}
